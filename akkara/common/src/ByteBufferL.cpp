308c9dfa207d13f3716944875f05fde85574c139
RiriFa <main@ririfa.net>
2025-12-05 00:20:14 +0900

    bro, it's CXX.

--- akkara/common/src/ByteBufferL.cpp
+++
@@ -1,6 +0,0 @@
-#include <akkaradb/common/ByteBufferL.h>
-
-// Implementation is all inline in header for performance
-// This file exists only to satisfy CMake requirements
-
-namespace akkaradb::common {}
